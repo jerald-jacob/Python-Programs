2576f4f342cac887c25e890c7733fe45ad9ea395
Jerald Jacob <jeraldjacob10@gmail.com>
2018-11-10 21:49:10 +0530

    weekend Medals count

---
+++ Milk - Medals/weekendMedals.py
@@ -0,0 +1,19 @@
+for i in range(7):
+    if(i==0):
+        m=4
+    elif(i==2):
+        m+=4
+    elif(i==5):
+        m+=4
+    elif(i==1):
+        m+=3
+    elif(i==3):
+        m+=3
+    elif(i==4):
+        m+=3
+    elif(i==6):
+        m+=3
+    else:
+        break;
+print("The Number of medals achieved in the weekend is ",m)
+   
